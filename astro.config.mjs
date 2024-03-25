8e87eedc36ad381b72807017275e05e6ca13bb4e
toyskip725 <toyskip725@gmail.com>
2024-03-25 21:39:16 +0900

    [feature] add markdown Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex]
+  },
   integrations: [react(), mdx()]
 });

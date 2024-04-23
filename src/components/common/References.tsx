a1e6e4a4fd393630185a1952c9a87f9e898dfc30
toyskip725 <toyskip725@gmail.com>
2024-04-23 23:20:57 +0900

    [refactor] delete unused inline style

--- src/components/common/References.tsx
+++ src/components/common/References.tsx
@@ -6,12 +6,7 @@ const References = ({ children }: Props) => {
   const mainStyle: React.CSSProperties = {
     padding: "0px 0px 20px 0px",
   };
-
-  const headerStyle: React.CSSProperties = {
-    fontSize: "1.2em",
-    fontWeight: "bold",
-  };
-
+  
   const contentStyle: React.CSSProperties = {
     fontSize: "0.9em",
     lineHeight: "1.75em",

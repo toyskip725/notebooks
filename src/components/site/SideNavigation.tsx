fb2d6451fd0f2c73a81d07c79be602be77f3250f
toyskip725 <toyskip725@gmail.com>
2024-05-05 17:07:37 +0900

    [feature] update Sidenavigation style

--- src/components/site/SideNavigation.tsx
+++ src/components/site/SideNavigation.tsx
@@ -7,21 +7,41 @@ type Props = {
 
 const SideNavigation = ({ dirpath, posts }: Props) => {
   const displayPosts = posts.sort((post1, post2) => post1.frontmatter.section - post2.frontmatter.section);
-  
-  const homeButtonStyle: React.CSSProperties = {
-    display: "block",
-    fontSize: "1.5em",
+
+  const mainStyle: React.CSSProperties = {
+    padding: "20px 0px",
+  };
+  const headerStyle: React.CSSProperties = {
+    padding: "0px",
+    margin: "0px",
+  };
+  const listStyle: React.CSSProperties = {
+    paddingLeft: "0px",
+  };
+  const linkElementStyle: React.CSSProperties = {
+    listStyle: "none",
+  };
+  const linkTextStyle: React.CSSProperties = {
     color: "#000",
-    margin: "20px 20px 20px 0px",
-    textAlign: "left",
-    textDecoration: "none"
+    textDecoration: "none",
+  };
+  const dividerStyle: React.CSSProperties = {
+    width: "30%",
+    margin: "7px 0px",
+    borderTop: "1px dotted #999",
   };
 
   return (
-    <div>
-      <a style={homeButtonStyle} href={import.meta.env.BASE_URL}>Notebooks</a>
-      <p>{dirpath}</p>
-      {displayPosts.map((post) => (<li><a href={post.url}>{post.frontmatter.title}</a></li>))}
+    <div style={mainStyle}>
+      <div style={dividerStyle}/>
+      {displayPosts.map((post) => (
+        <>
+          <li style={linkElementStyle}>
+            <a style={linkTextStyle} href={post.url}>{post.frontmatter.title}</a>
+          </li>
+          <div style={dividerStyle}/>
+        </>
+      ))}
     </div>
   );
 };

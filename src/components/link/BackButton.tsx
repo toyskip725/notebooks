57fdab6d7748cd9b4e687771494446e5b53a6c81
toyskip725 <toyskip725@gmail.com>
2024-08-10 17:47:43 +0900

    [feature] update BackButton

--- src/components/link/BackButton.tsx
+++ src/components/link/BackButton.tsx
@@ -1,25 +1,26 @@
 type Props = {
-  name: string;
   href: string;
 };
 
-const BackButton = ({ name, href }: Props) => {
+const BackButton = ({ href }: Props) => {
   const layoutStyle: React.CSSProperties = {
+    width: "fit-content",
     margin: "20px 0px",
+    border: "1px solid #000",
   };
   const linkStyle: React.CSSProperties = {
     display: "inline-flex",
     alignItems: "center",
     color: "#000",
     textDecoration: "none",
-    fontSize: "0.8em",
+    padding: "7px 10px 7px 5px",
   };
 
   return (
     <div style={layoutStyle}>
       <a href={href} style={linkStyle}>
-        <span className="material-symbols-outlined">arrow_back</span> 
-        <span>{name}に戻る</span>
+        <span className="material-symbols-outlined">arrow_back_2</span> 
+        <span>戻る</span>
       </a>
     </div>
   );

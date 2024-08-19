8db8e55a3224b19c0bb171f4c2a7a932297d3836
toyskip725 <toyskip725@gmail.com>
2024-08-20 00:00:34 +0900

    [feature] locate banner in NoteHeader

--- src/components/note/NoteHeader.tsx
+++ src/components/note/NoteHeader.tsx
@@ -1,12 +1,15 @@
 import MainColorDivider from "../design/MainColorDivider";
+import NoteSegmentBanner from "./NoteSegmentBanner";
 
 type Props = {
   title: string;
+  pathname: string;
 };
 
-const NoteHeader = ({ title }: Props) => {
+const NoteHeader = ({ title, pathname }: Props) => {
   return (
     <div>
+      <NoteSegmentBanner path={pathname} />
       <h1>{title}</h1>
       <MainColorDivider />
     </div>

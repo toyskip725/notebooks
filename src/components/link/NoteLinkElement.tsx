a091095924540e0de7126f94a18dc9742c7f68e1
toyskip725 <toyskip725@gmail.com>
2024-09-06 23:39:01 +0900

    [feature] create NoteLinkElelment component

---
+++ src/components/link/NoteLinkElement.tsx
@@ -0,0 +1,37 @@
+type Props = {
+  segment: string;
+  section: number;
+  path: string;
+  title: string;
+};
+
+const NoteLinkElement = ({ segment, section, path, title }: Props) => {
+  const href = `${import.meta.env.SITE}${import.meta.env.BASE_URL}/${segment}/${path}`;
+
+  const mainStyle: React.CSSProperties = {
+    listStyle: "none",
+  };
+  const numberStyle = {
+    padding: "0px 10px",
+    margin: "4px",
+  };
+  const linkStyle: React.CSSProperties = {
+    display: "inline-block",
+  };
+  const sectionStyle: React.CSSProperties = {
+    padding: "0em 1em",
+  };
+
+  return (
+    <li style={mainStyle}>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <span style={numberStyle}>{section}</span>
+        <a href={href} style={linkStyle}>
+          <span style={sectionStyle}>{title}</span>
+        </a>
+      </div>
+    </li>
+  );
+};
+
+export default NoteLinkElement;

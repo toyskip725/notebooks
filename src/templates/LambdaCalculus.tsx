13f508ddf61bcf537b05560e70e9c95024d2e00f
toyskip725 <toyskip725@gmail.com>
2024-09-15 00:07:47 +0900

    [feature] update index

--- src/templates/LambdaCalculus.tsx
+++ src/templates/LambdaCalculus.tsx
@@ -11,6 +11,8 @@ const LambdaCalculus = () => {
       <ul style={listStyle}>
         <NoteLinkElement segment={PATH} section={1} path={"untyped-lambda-calculus"} title={"型なしラムダ計算"} />
         <NoteLinkElement segment={PATH} section={2} path={"church-rosser-theorem"} title={"チャーチ・ロッサーの定理"} />
+        <NoteLinkElement segment={PATH} section={3} path={""} title={"コンビネータ論理 (1)"} />
+        <NoteLinkElement segment={PATH} section={4} path={""} title={"コンビネータ論理 (2)"} />
       </ul>
     </div>
   );
